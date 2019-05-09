9ba037d76af80d440cbd16c5ef6123b73d754ebe
qingzheng.li <liqz32@chinaunicom.cn>
2019-05-09 16:03:36 +0800

    格式化

--- cloud_parent/cloud_business/src/main/java/com/ffxl/cloud/service/impl/STimetaskLogServiceImpl.java
+++ cloud_parent/cloud_business/src/main/java/com/ffxl/cloud/service/impl/STimetaskLogServiceImpl.java
@@ -7,47 +7,46 @@ import com.ffxl.cloud.model.base.BaseSTimetaskLogExample.Criteria;
 import com.ffxl.cloud.service.STimetaskLogService;
 import com.ffxl.platform.core.GenericMapper;
 import com.ffxl.platform.core.GenericServiceImpl;
-
-import java.util.Date;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
- /**
+import java.util.Date;
+import java.util.List;
+
+/**
  * Generate By MBG for serviceImpl
  **/
 @Service
 public class STimetaskLogServiceImpl extends GenericServiceImpl<STimetaskLog, STimetaskLogExample, String> implements STimetaskLogService {
     private static final Logger LOGGER = LoggerFactory.getLogger(STimetaskLogServiceImpl.class);
-
+    
     @Autowired
     private STimetaskLogMapper sTimetaskLogMapper;
-
+    
     @Override
     public GenericMapper<STimetaskLog, STimetaskLogExample, String> getGenericMapper() {
         return sTimetaskLogMapper;
     }
-
+    
     public STimetaskLog queryByModel(STimetaskLog sTimetaskLog) {
         STimetaskLogExample example = new STimetaskLogExample();
-        Criteria c= example.createCriteria();
-        List<STimetaskLog> modelList =  sTimetaskLogMapper.selectByExample(example);
-        if(modelList.size() > 0){
+        Criteria c = example.createCriteria();
+        List<STimetaskLog> modelList = sTimetaskLogMapper.selectByExample(example);
+        if(modelList.size() > 0) {
             return modelList.get(0);
-        }else{
+        } else {
             return null;
         }
     }
-
-	@Override
-	public int deleteLog(Date deleteDateStr) {
-		STimetaskLogExample example = new STimetaskLogExample();
-		Criteria c= example.createCriteria();
-		c.andCreateDateLessThanOrEqualTo(deleteDateStr);
-		int ret = sTimetaskLogMapper.deleteByExample(example);
-		return ret;
-	}
+    
+    @Override
+    public int deleteLog(Date deleteDateStr) {
+        STimetaskLogExample example = new STimetaskLogExample();
+        Criteria c = example.createCriteria();
+        c.andCreateDateLessThanOrEqualTo(deleteDateStr);
+        int ret = sTimetaskLogMapper.deleteByExample(example);
+        return ret;
+    }
 }
