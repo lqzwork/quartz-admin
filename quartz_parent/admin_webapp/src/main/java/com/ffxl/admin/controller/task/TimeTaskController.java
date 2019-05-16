90f1a8056db748ba9c50b540a2c09c688208bb23
liyj <704603154@qq.com>
2019-05-16 09:11:22 +0800

    获取任务列表接口修改

--- quartz_parent/admin_webapp/src/main/java/com/ffxl/admin/controller/task/TimeTaskController.java
+++ quartz_parent/admin_webapp/src/main/java/com/ffxl/admin/controller/task/TimeTaskController.java
@@ -1,15 +1,14 @@
 package com.ffxl.admin.controller.task;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpSession;
-
+import com.ffxl.admin.controller.BaseController;
+import com.ffxl.admin.util.DataTablesUtil;
+import com.ffxl.business.service.STimetaskService;
+import com.ffxl.common.constant.Const;
+import com.ffxl.common.core.Page;
+import com.ffxl.common.exception.BusinessException;
+import com.ffxl.common.util.*;
+import com.ffxl.dao.model.STimetask;
+import com.ffxl.dao.model.warpper.ScheduleJob;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import net.sf.json.JsonConfig;
@@ -25,21 +24,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.ffxl.admin.controller.BaseController;
-import com.ffxl.admin.util.DataTablesUtil;
-import com.ffxl.dao.model.STimetask;
-import com.ffxl.dao.model.warpper.ScheduleJob;
-import com.ffxl.business.service.STimetaskService;
-import com.ffxl.common.constant.Const;
-import com.ffxl.common.core.Page;
-import com.ffxl.common.exception.BusinessException;
-import com.ffxl.common.util.CronUtil;
-import com.ffxl.common.util.DateUtil;
-import com.ffxl.common.util.HttpConnectUtil;
-import com.ffxl.common.util.JsonDateValueProcessor;
-import com.ffxl.common.util.JsonResult;
-import com.ffxl.common.util.Message;
-import com.ffxl.common.util.StringUtil;
+import javax.servlet.http.HttpSession;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * 定时任务
@@ -49,7 +37,12 @@ import com.ffxl.common.util.StringUtil;
 @Controller
 @RequestMapping(value = "/task")
 public class TimeTaskController extends BaseController {
-    // private  static String JOB_URL =  Const.QUARTZ_JOB_URL;
+
+    private static final Logger logger = LoggerFactory.getLogger(TimeTaskController.class);
+    @Autowired
+    private STimetaskService stimetaskService;
+
+//    private  static String JOB_URL =  Const.QUARTZ_JOB_URL;
     private  static String JOB_URL =  "http://localhost:8099";
     private  static String ALL_JOB = JOB_URL+"/opt/getAllJob"; //所有计划中的任务列表
     private  static String RUNNING_JOB = JOB_URL+"/opt/getRunningJob";//所有正在运行的job
@@ -60,175 +53,154 @@ public class TimeTaskController extends BaseController {
     private  static String RUNA_JOB =JOB_URL+ "/opt/runAJobNow";//立即执行job
     private  static String UPDATE_JOB = JOB_URL+"/opt/updateJobCron";//更新job时间表达式
     
-    
-	private static final Logger logger = LoggerFactory.getLogger(TimeTaskController.class);
-	@Autowired
-    private STimetaskService stimetaskService;
-
-	
-	@InitBinder
+    @InitBinder
     public void initBinder(WebDataBinder binder) {
         DateFormat fmt = new SimpleDateFormat(DateUtil.STANDARD_DATE_FORMAT_STR);
         CustomDateEditor dateEditor = new CustomDateEditor(fmt, true);
         binder.registerCustomEditor(Date.class, dateEditor);
     }
 
-	/**
-	 * 列表页面跳转
-	 * @return
-	 */
-	@RequestMapping(value="/list")
-	public ModelAndView userList(STimetask task){
-		ModelAndView mv = this.getModelAndView();
-		mv.setViewName("system/timeTaskList");
-		return mv;
-	}
+    /**
+     * 列表页面跳转
+     *
+     * @return
+     */
+    @RequestMapping(value = "/list")
+    public ModelAndView userList(STimetask task) {
+        ModelAndView mv = this.getModelAndView();
+        mv.setViewName("system/timeTaskList");
+        return mv;
+    }
 
-	/**
-	 * 列表
-	 * @return
-	 */
-	@RequestMapping(value="/task_list")
-	@ResponseBody
-	public JsonResult taskList(DataTablesUtil dataTables, STimetask task, Page page, HttpSession session){
-		 List<STimetask> list = stimetaskService.selectByPage(task, page);
-		 
-		 // 查询task的运行状态
-		 String result = HttpConnectUtil.httpRequest(RUNNING_JOB, Const.REQUEST_METHOD_POST, null);
-		 if(result!=null){
-		 JSONObject jsonResult = JSONObject.fromObject(result);
-		 Map<String, ScheduleJob> map = new HashMap();
-		 if( jsonResult.get("code").equals("2000")){
-		   JSONObject js = (JSONObject) jsonResult.get("data");
-		   JSONArray dataArray = (JSONArray) js.get("job");
-		   if(dataArray.size() > 0){
-		     List<ScheduleJob> jobList =  JSONArray.toList(dataArray,ScheduleJob.class);
-	           for(ScheduleJob job: jobList){
-	             map.put(job.getJobId(), job);
-	           }
-		   }
-		 }
-  		 for(STimetask st: list){
-    		   if(map.containsKey(st.getId())){
-    		     st.setConcurrent(true);
-    		   }
-    		 }
-		 }
-		 
-		 // 查询task的计划状态
-         String planResult = HttpConnectUtil.httpRequest(ALL_JOB, Const.REQUEST_METHOD_GET, null);
-         if(planResult!=null){
-         JSONObject jsonPlanResult = JSONObject.fromObject(planResult);
-         Map<String, ScheduleJob> planMap = new HashMap();
-         if(jsonPlanResult.get("code").equals("2000")){
-           JSONObject js = (JSONObject) jsonPlanResult.get("data");
-           JSONArray dataArray = (JSONArray) js.get("job");
-           if(dataArray.size() > 0){
-             List<ScheduleJob> jobList =  JSONArray.toList(dataArray,ScheduleJob.class);
-             for(ScheduleJob job: jobList){
-               planMap.put(job.getJobId(), job);
-             }
-           }
-         }
-         for(STimetask st: list){
-               if(planMap.containsKey(st.getId())){
-                 String status = planMap.get(st.getId()).getJobStatus();
-                 st.setPlanStatus(status);
-               }
-             }
-           }
-		  //返回dataTable所需数据
-	     dataTables = this.getDataTables(page, dataTables, list);
+    /**
+     * 列表
+     *
+     * @return
+     */
+    @RequestMapping(value = "/task_list")
+    @ResponseBody
+    public JsonResult taskList(DataTablesUtil dataTables, STimetask task, Page page, HttpSession session) {
+        List<STimetask> list = stimetaskService.selectByPage(task, page);
+        /**
+         * 查询所有任务
+         */
+        String planResult = HttpConnectUtil.httpRequest(ALL_JOB, Const.REQUEST_METHOD_GET, null);
+        if (planResult != null) {
+            JSONObject jsonPlanResult = JSONObject.fromObject(planResult);
+            Map<String, ScheduleJob> planMap = new HashMap<String, ScheduleJob>();
+            if (jsonPlanResult.get("code").equals("2000")) {
+                JSONObject js = (JSONObject) jsonPlanResult.get("data");
+                JSONArray dataArray = (JSONArray) js.get("job");
+                if (dataArray.size() > 0) {
+                    List<ScheduleJob> jobList = JSONArray.toList(dataArray, ScheduleJob.class);
+                    for (ScheduleJob job : jobList) {
+                        planMap.put(job.getJobId(), job);
+                    }
+                }
+            }
+            for (STimetask st : list) {
+                if (planMap.containsKey(st.getId())) {
+                    st.setPlanStatus(planMap.get(st.getId()).getJobStatus());
+                }
+            }
+        }
+        //返回dataTable所需数据
+        dataTables = this.getDataTables(page, dataTables, list);
         return new JsonResult("2000", dataTables);
-	}
-	
-	/**
+    }
+
+    /**
      * 立即执行一次job
      * 用于测试任务是否正确
+     *
      * @param id
      * @return
      */
-    @RequestMapping(value="/run_task2job")
+    @RequestMapping(value = "/run_task2job")
     @ResponseBody
-    public JsonResult run_task2job(String id){
-      //查询task
-      STimetask stimetask = stimetaskService.selectByPrimaryKey(id);
-      JsonConfig jsonConfig = new JsonConfig();
-      jsonConfig.registerJsonValueProcessor(Date.class, new JsonDateValueProcessor());
-      JSONObject jsonArray = JSONObject.fromObject(stimetask,jsonConfig);
-      String result = HttpConnectUtil.httpRequest(RUNA_JOB, Const.REQUEST_METHOD_POST, jsonArray.toString());
-      logger.info(result);
-      if(result ==null){
-        return new JsonResult("5000", "定时项目未启动",null);
-      }else{
-        return new JsonResult("2000", null);
-      }
+    public JsonResult run_task2job(String id) {
+        //查询task
+        STimetask stimetask = stimetaskService.selectByPrimaryKey(id);
+        JsonConfig jsonConfig = new JsonConfig();
+        jsonConfig.registerJsonValueProcessor(Date.class, new JsonDateValueProcessor());
+        JSONObject jsonArray = JSONObject.fromObject(stimetask, jsonConfig);
+        String result = HttpConnectUtil.httpRequest(RUNA_JOB, Const.REQUEST_METHOD_POST, jsonArray.toString());
+        logger.info(result);
+        if (result == null) {
+            return new JsonResult("5000", "定时项目未启动", null);
+        } else {
+            return new JsonResult("2000", null);
+        }
     }
-	
-	/**
-	 * 添加job到计划列表
-	 * @param id
-	 * @return
-	 */
-	@RequestMapping(value="/add_task2job")
+
+    /**
+     * 添加job到计划列表
+     *
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "/add_task2job")
     @ResponseBody
-    public JsonResult add_task2job(String id){
-	  //查询task
-	  STimetask stimetask = stimetaskService.selectByPrimaryKey(id);
-	  JsonConfig jsonConfig = new JsonConfig();
-      jsonConfig.registerJsonValueProcessor(Date.class, new JsonDateValueProcessor());
-      JSONObject jsonArray = JSONObject.fromObject(stimetask,jsonConfig);
-	  String result = HttpConnectUtil.httpRequest(ADD_JOB, Const.REQUEST_METHOD_POST, jsonArray.toString());
-	  logger.info(result);
-	  if(result ==null){
-	    return new JsonResult("5000", "定时项目未启动",null);
-	  }else{
-	    return new JsonResult("2000", null);
-	  }
-	 
+    public JsonResult add_task2job(String id) {
+        //查询task
+        STimetask stimetask = stimetaskService.selectByPrimaryKey(id);
+        JsonConfig jsonConfig = new JsonConfig();
+        jsonConfig.registerJsonValueProcessor(Date.class, new JsonDateValueProcessor());
+        JSONObject jsonArray = JSONObject.fromObject(stimetask, jsonConfig);
+        String result = HttpConnectUtil.httpRequest(ADD_JOB, Const.REQUEST_METHOD_POST, jsonArray.toString());
+        logger.info(result);
+        if (result == null) {
+            return new JsonResult("5000", "定时项目未启动", null);
+        } else {
+            return new JsonResult("2000", null);
+        }
+
     }
-	
-	/**
+
+    /**
      * 从计划列表中暂停job
+     *
      * @param id
      * @return
      */
-    @RequestMapping(value="/stop_task2job")
+    @RequestMapping(value = "/stop_task2job")
     @ResponseBody
-    public JsonResult stop_task2job(String id){
-      //查询task
-      STimetask stimetask = stimetaskService.selectByPrimaryKey(id);
-      JsonConfig jsonConfig = new JsonConfig();
-      jsonConfig.registerJsonValueProcessor(Date.class, new JsonDateValueProcessor());
-      JSONObject jsonArray = JSONObject.fromObject(stimetask,jsonConfig);
-      String result = HttpConnectUtil.httpRequest(PAUSE_JOB, Const.REQUEST_METHOD_POST, jsonArray.toString());
-      logger.info(result);
-      if(result ==null){
-        return new JsonResult("5000", "定时项目未启动",null);
-      }else{
-        return new JsonResult("2000", null);
-      }
+    public JsonResult stop_task2job(String id) {
+        //查询task
+        STimetask stimetask = stimetaskService.selectByPrimaryKey(id);
+        JsonConfig jsonConfig = new JsonConfig();
+        jsonConfig.registerJsonValueProcessor(Date.class, new JsonDateValueProcessor());
+        JSONObject jsonArray = JSONObject.fromObject(stimetask, jsonConfig);
+        String result = HttpConnectUtil.httpRequest(PAUSE_JOB, Const.REQUEST_METHOD_POST, jsonArray.toString());
+        logger.info(result);
+        if (result == null) {
+            return new JsonResult("5000", "定时项目未启动", null);
+        } else {
+            return new JsonResult("2000", null);
+        }
     }
+
     /**
      * 从计划列表中移除job
+     *
      * @param id
      * @return
      */
-    @RequestMapping(value="/remove_task2job")
+    @RequestMapping(value = "/remove_task2job")
     @ResponseBody
-    public JsonResult remove_task2job(String id){
-      //查询task
-      STimetask stimetask = stimetaskService.selectByPrimaryKey(id);
-      JsonConfig jsonConfig = new JsonConfig();
-      jsonConfig.registerJsonValueProcessor(Date.class, new JsonDateValueProcessor());
-      JSONObject jsonArray = JSONObject.fromObject(stimetask,jsonConfig);
-      String result = HttpConnectUtil.httpRequest(DELETE_JOB, Const.REQUEST_METHOD_POST, jsonArray.toString());
-      logger.info(result);
-      if(result ==null){
-        return new JsonResult("5000", "定时项目未启动",null);
-      }else{
-        return new JsonResult("2000", null);
-      }
+    public JsonResult remove_task2job(String id) {
+        //查询task
+        STimetask stimetask = stimetaskService.selectByPrimaryKey(id);
+        JsonConfig jsonConfig = new JsonConfig();
+        jsonConfig.registerJsonValueProcessor(Date.class, new JsonDateValueProcessor());
+        JSONObject jsonArray = JSONObject.fromObject(stimetask, jsonConfig);
+        String result = HttpConnectUtil.httpRequest(DELETE_JOB, Const.REQUEST_METHOD_POST, jsonArray.toString());
+        logger.info(result);
+        if (result == null) {
+            return new JsonResult("5000", "定时项目未启动", null);
+        } else {
+            return new JsonResult("2000", null);
+        }
     }
 	
     /**
@@ -241,8 +213,8 @@ public class TimeTaskController extends BaseController {
     public JsonResult update_task(String ids,String type){
       //查询task
       String[] idArray = ids.split(",");
-      Map<String,String> selectedIdMap =  new HashMap();
-      List<String> idList = new ArrayList();
+      Map<String,String> selectedIdMap =  new HashMap<String,String>();
+      List<String> idList = new ArrayList<String>();
       for (int i = 0; i < idArray.length; i++) {
           idList.add(idArray[i]);
       }
@@ -253,40 +225,39 @@ public class TimeTaskController extends BaseController {
           return new JsonResult(false);
       }
     }
-    
+
     /**
      * 删除job
-     * @param id
+     *
+     * @param ids
      * @return
      */
-    @RequestMapping(value="/delete_task")
+    @RequestMapping(value = "/delete_task")
     @ResponseBody
-    public JsonResult delete_task(String ids){
-      //查询task
-      String[] idArray = ids.split(",");
-      Map<String,String> selectedIdMap =  new HashMap();
-      List<String> idList = new ArrayList();
-      for (int i = 0; i < idArray.length; i++) {
-          idList.add(idArray[i]);
-      }
-      int ret = stimetaskService.deleteByIds(idList);
-      if(ret >0){
-          return new JsonResult(true);
-      }else{
-          return new JsonResult(false);
-      }
+    public JsonResult delete_task(String ids) {
+        //查询task
+        String[] idArray = ids.split(",");
+        Map<String, String> selectedIdMap = new HashMap<String, String>();
+        List<String> idList = new ArrayList<String>();
+        for (int i = 0; i < idArray.length; i++) {
+            idList.add(idArray[i]);
+        }
+        int ret = stimetaskService.deleteByIds(idList);
+        if (ret > 0) {
+            return new JsonResult(true);
+        } else {
+            return new JsonResult(false);
+        }
     }
-	
-	
-	
-	
-	
-	/**
+
+
+    /**
      * 详情页面
+     *
      * @return
      */
-    @RequestMapping(value="/task_detail")
-    public ModelAndView detail(String id){
+    @RequestMapping(value = "/task_detail")
+    public ModelAndView detail(String id) {
         ModelAndView mv = this.getModelAndView();
         STimetask model = new STimetask();
         model = stimetaskService.selectByPrimaryKey(id);
@@ -294,72 +265,75 @@ public class TimeTaskController extends BaseController {
         mv.setViewName("system/timeTaskDetail");
         return mv;
     }
+
     /**
      * 解析cron
+     *
      * @return
      */
-    @RequestMapping(value="/analysis_cron")
+    @RequestMapping(value = "/analysis_cron")
     @ResponseBody
-    public JsonResult analysisCron(String cron){
+    public JsonResult analysisCron(String cron) {
         try {
-          Date date = new Date();
-          String dateStr = DateUtil.formatStandardDatetime(date);
-          List<String> dateList = CronUtil.cronAlgBuNums(cron, dateStr, 5);
-          return new JsonResult("2000", dateList);
+            Date date = new Date();
+            String dateStr = DateUtil.formatStandardDatetime(date);
+            List<String> dateList = CronUtil.cronAlgBuNums(cron, dateStr, 5);
+            return new JsonResult("2000", dateList);
         } catch (Exception e) {
-          e.printStackTrace();
-          return new JsonResult("5000", null);
+            e.printStackTrace();
+            return new JsonResult("5000", null);
         }
     }
-    
+
     /**
      * 验证名称是否存在
+     *
      * @param id
      * @param groupName
      * @param name
      * @return
-     *
      */
-    @RequestMapping(value="/check_name")
+    @RequestMapping(value = "/check_name")
     @ResponseBody
-    public Boolean check_name(String id, String groupName, String name){
-      if(StringUtil.isEmpty(groupName,name)){
-        throw new BusinessException(Message.M4003);
-      }
-      STimetask task = new STimetask();
-      task.setId(id);
-      task.setGroupName(groupName);
-      task.setName(name);
-      STimetask queryTask = stimetaskService.checkName(task);
-      if(queryTask !=null){
-        logger.debug("组.任务名 exists,return false");
-        return false;
-      }else{
-        logger.debug("组.任务名 not exists,return true");
-        return true;
-      }
+    public Boolean check_name(String id, String groupName, String name) {
+        if (StringUtil.isEmpty(groupName, name)) {
+            throw new BusinessException(Message.M4003);
+        }
+        STimetask task = new STimetask();
+        task.setId(id);
+        task.setGroupName(groupName);
+        task.setName(name);
+        STimetask queryTask = stimetaskService.checkName(task);
+        if (queryTask != null) {
+            logger.debug("组.任务名 exists,return false");
+            return false;
+        } else {
+            logger.debug("组.任务名 not exists,return true");
+            return true;
+        }
     }
-    
+
     /**
      * 保存
+     *
      * @return
      */
-    @RequestMapping(value="/task_save")
+    @RequestMapping(value = "/task_save")
     @ResponseBody
-    public JsonResult userSave(STimetask task,  HttpSession session){
+    public JsonResult userSave(STimetask task, HttpSession session) {
         //获取系统操作人员
-    	String longName = "admin";
+        String longName = "admin";
         task.setModifyUserId(longName);
-        try{
-            int ret= stimetaskService.insertOrUpdateByUser(task,longName);
-            if(ret > 0){
-                return new JsonResult("2000",task);
-            }else{
+        try {
+            int ret = stimetaskService.insertOrUpdateByUser(task, longName);
+            if (ret > 0) {
+                return new JsonResult("2000", task);
+            } else {
                 return new JsonResult("5000");
             }
-        }catch(BusinessException e){
-            return new JsonResult("5001",e.getMessage(),null);
-        }    
+        } catch (BusinessException e) {
+            return new JsonResult("5001", e.getMessage(), null);
+        }
     }
-	
+
 }
